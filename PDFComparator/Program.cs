d4db19eef8e0e4bfffbe7e3affe3b69c75157b8c
Максим Кондратович <kondratovich4444@gmail.com>
2018-12-11 13:01:32 +0300

    Some updates

--- PDFComparator/Program.cs
+++ PDFComparator/Program.cs
@@ -15,26 +15,8 @@ namespace PDFComparator
     {
         static void Main(string[] args)
         {
-            FileStream fs = new FileStream("Chapter1_Example1.pdf", FileMode.Create, FileAccess.Write, FileShare.None); 
-
-            Document doc = new Document();
-            PdfWriter writer = PdfWriter.GetInstance(doc, fs);
-            doc.Open();
-            doc.Add(new Paragraph("Hello World"));
-            doc.Add(new Paragraph("ho ho ho ho"));
-            doc.Close();
-
-            FileStream fs1 = new FileStream("Chapter1_Example.pdf", FileMode.Create, FileAccess.Write, FileShare.None);
-            Document doc1 = new Document();
-            PdfWriter writer1 = PdfWriter.GetInstance(doc1, fs1);
-            doc1.Open();
-            doc1.Add(new Paragraph("Hello World"));
-            doc1.Add(new Paragraph("ha ha ha ha"));
-            doc1.Close();
-
 
             CompareTwoPDF("TablesSource.pdf", "TablesTarget.pdf");
-
             Console.ReadLine();
         }
         public static string ReadPdfFile(string fileName)
@@ -59,6 +41,70 @@ namespace PDFComparator
         static string FirstFile, SecondFile;
         public static void CompareTwoPDF(string FirstPDF, string SecondPDF)
         {
+            IsExist(FirstPDF, SecondPDF);
+
+            List<string> File1diff;
+            List<string> File2diff;
+
+            IEnumerable<string> file1 = FirstFile.Trim().Split('\r', '\n');
+            IEnumerable<string> file2 = SecondFile.Trim().Split('\r', '\n');
+
+            File1diff = file1.ToList();
+            File2diff = file2.ToList();
+
+            if (file2.Count() > file1.Count())
+            {
+                Console.WriteLine("The FirstPDF has less number of lines than the SecondPDF.");
+                for (int i = 0; i < File1diff.Count; i++)
+                {
+                    if (!File1diff[i].Equals(File2diff[i]))
+                    {
+                        Console.WriteLine("The FirstPDF content: " + File1diff[i] + "\r\n" + "The SecondPDF content: " + File2diff[i]);
+                    }
+
+                }
+
+                for (int i = File1diff.Count; i < File2diff.Count; i++)
+                {
+                    Console.WriteLine("File 2 extra content: " + File2diff[i]);
+                }
+
+            }
+            else if (file2.Count() < file1.Count())
+            {
+                Console.WriteLine("The SecondPDF has less number of lines than File 1.");
+
+                for (int i = 0; i < File2diff.Count; i++)
+                {
+                    if (!File1diff[i].Equals(File2diff[i]))
+                    {
+                        Console.WriteLine("The FirstPDF content: " + File1diff[i] + "\r\n" + "The SecondPDF content: " + File2diff[i]);
+                    }
+
+                }
+
+                for (int i = File2diff.Count; i < File1diff.Count; i++)
+                {
+                    Console.WriteLine("The FirstPDF extra content: " + File1diff[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("The FirstPDF and the SecondPDF, both are having same number of lines.");
+
+                for (int i = 0; i < File1diff.Count; i++)
+                {
+                    if (!File1diff[i].Equals(File2diff[i]))
+                    {
+                        Console.WriteLine("The FirstPDF content: " + File1diff[i] + "\r\n" + "The SecondPDF content: " + File2diff[i]);
+                    }
+
+                }
+
+            }
+        }
+        private static void IsExist(String FirstPDF, String SecondPDF)
+        {
             if (File.Exists(FirstPDF) && File.Exists(SecondPDF))
             {
                 PdfReader reader = new PdfReader(FirstPDF);
@@ -77,66 +123,6 @@ namespace PDFComparator
             else
             {
                 Console.WriteLine("Files does not exist.");
-            }
-
-            List<string> File1diff;
-            List<string> File2diff;
-
-            IEnumerable<string> file1 = FirstFile.Trim().Split('\r', '\n');
-            IEnumerable<string> file2 = SecondFile.Trim().Split('\r', '\n');
-
-            File1diff = file1.ToList();
-            File2diff = file2.ToList();
-
-            if (file2.Count() > file1.Count())
-            {
-                Console.WriteLine("File 1 has less number of lines than File 2.");
-                for (int i = 0; i < File1diff.Count; i++)
-                {
-                    if (!File1diff[i].Equals(File2diff[i]))
-                    {
-                        Console.WriteLine("File 1 content: " + File1diff[i] + "\r\n" + "File 2 content: " + File2diff[i]);
-                    }
-
-                }
-
-                for (int i = File1diff.Count; i < File2diff.Count; i++)
-                {
-                    Console.WriteLine("File 2 extra content: " + File2diff[i]);
-                }
-
-            }
-            else if (file2.Count() < file1.Count())
-            {
-                Console.WriteLine("File 2 has less number of lines than File 1.");
-
-                for (int i = 0; i < File2diff.Count; i++)
-                {
-                    if (!File1diff[i].Equals(File2diff[i]))
-                    {
-                        Console.WriteLine("File 1 content: " + File1diff[i] + "\r\n" + "File 2 content: " + File2diff[i]);
-                    }
-
-                }
-
-                for (int i = File2diff.Count; i < File1diff.Count; i++)
-                {
-                    Console.WriteLine("File 1 extra content: " + File1diff[i]);
-                }
-            }
-            else
-            {
-                Console.WriteLine("File 1 and File 2, both are having same number of lines.");
-
-                for (int i = 0; i < File1diff.Count; i++)
-                {
-                    if (!File1diff[i].Equals(File2diff[i]))
-                    {
-                        Console.WriteLine("File 1 content: " + File1diff[i] + "\r\n" + "File 2 Content: " + File2diff[i]);
-                    }
-
-                }
-
             }
         }
     }
